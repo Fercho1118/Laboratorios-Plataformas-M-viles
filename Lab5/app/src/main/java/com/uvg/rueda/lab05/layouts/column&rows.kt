a9dd777524ac59e3edbba4d4cb18ea94935d06d8
Fercho1118 <rue23748@uvg.edu.gt>
2024-08-08 20:30:27 -0600

    Sección de cumpleaños
    Se programó la sección donde va el texto con mi cumpleaños y un botón que de momento no tiene funcionalidad.

--- Lab5/app/src/main/java/com/uvg/rueda/lab05/layouts/column&rows.kt
+++ Lab5/app/src/main/java/com/uvg/rueda/lab05/layouts/column&rows.kt
@@ -10,12 +10,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Update
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -23,6 +27,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.room.Update
@@ -41,7 +46,7 @@ fun MyApp() {
                 .fillMaxWidth()
                 .background(Color.Gray)
                 .padding(8.dp),
-            horizontalArrangement = Arrangement.Absolute.SpaceBetween,
+            horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ){
             Row(verticalAlignment = Alignment.CenterVertically) {
@@ -59,6 +64,21 @@ fun MyApp() {
                     context.startActivity(intent)
                 }
             )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Column{
+                Text(text = "Lunes", fontSize = 32.sp, fontWeight = FontWeight.Bold)
+                Text(text = "18 de noviembre", fontSize = 20.sp, color = Color.Gray)
+            }
+            OutlinedButton(onClick = {/*Nada de momento*/}, shape = CutCornerShape(0)) {
+                Text(text = "Terminar jornada")
+            }
         }
     }
 }
