29b30f6c3a11442b49bad82f3649c068b5a40a5d
Fercho1118 <rue23748@uvg.edu.gt>
2024-08-15 00:54:44 -0600

    Sección del título
    Se agregó el título con mi nombre en la parte superior.

---
+++ Lab6/app/src/main/java/com/uvg/rueda/laboratorio6/layouts/columns&rows.kt
@@ -0,0 +1,29 @@
+package com.uvg.rueda.laboratorio6.layouts
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun AppContadora() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ){
+        Text(
+            text = "Fernando Rueda",
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
