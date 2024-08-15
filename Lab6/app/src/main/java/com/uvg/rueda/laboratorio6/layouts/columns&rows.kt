55bb7fa59dbfe4c5e6f763cb12ac0cbd38c70fa1
Fercho1118 <rue23748@uvg.edu.gt>
2024-08-15 13:47:49 -0600

    Sección de estadísitcas
    Se agregó la sección donde se puede ver cantidad de incrementos, valor máximo, etc...

--- Lab6/app/src/main/java/com/uvg/rueda/laboratorio6/layouts/columns&rows.kt
+++ Lab6/app/src/main/java/com/uvg/rueda/laboratorio6/layouts/columns&rows.kt
@@ -23,6 +23,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -91,5 +95,45 @@ fun AppContadora() {
             }
         }
         Spacer(modifier = Modifier.height(32.dp))
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.Start
+        ){
+            Text(text = buildAnnotatedString {
+                append("Total incrementos:            ")
+                withStyle(style = SpanStyle(fontWeight = FontWeight.Normal)) {
+                    append(incrementroContador.toString())
+                }
+            }, fontSize = 28.sp, fontWeight = FontWeight.Bold, color = Color.Black
+            )
+            Text(text = buildAnnotatedString {
+                append("Total decrementos:           ")
+                withStyle(style = SpanStyle(fontWeight = FontWeight.Normal)) {
+                    append(decrementoContador.toString())
+                }
+            }, fontSize = 28.sp, fontWeight = FontWeight.Bold, color = Color.Black
+            )
+            Text(text = buildAnnotatedString {
+                append("Valor máximo:                    ")
+                withStyle(style = SpanStyle(fontWeight = FontWeight.Normal)) {
+                    append(valorMaximo.toString())
+                }
+            }, fontSize = 28.sp, fontWeight = FontWeight.Bold, color = Color.Black
+            )
+            Text(text = buildAnnotatedString {
+                append("Valor mínimo:                    ")
+                withStyle(style = SpanStyle(fontWeight = FontWeight.Normal)) {
+                    append(valorMinimo.toString())
+                }
+            }, fontSize = 28.sp, fontWeight = FontWeight.Bold, color = Color.Black
+            )
+            Text(text = buildAnnotatedString {
+                append("Total de cambios:              ")
+                withStyle(style = SpanStyle(fontWeight = FontWeight.Normal)) {
+                    append(cambioContador.toString())
+                }
+            }, fontSize = 28.sp, fontWeight = FontWeight.Bold, color = Color.Black
+            )
+        }
     }
 }
