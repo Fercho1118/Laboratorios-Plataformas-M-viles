0dd5ee5dd150ab7a83fc79b50a71f08bca0a300e
Fercho1118 <rue23748@uvg.edu.gt>
2024-08-15 20:54:27 -0600

    Historial
    Se agregó un FlowRow para agregar el historial con figuras circulares con esquinas cortadas y colores dependiendo si decremento o incrementa.

--- Lab6/app/src/main/java/com/uvg/rueda/laboratorio6/layouts/columns&rows.kt
+++ Lab6/app/src/main/java/com/uvg/rueda/laboratorio6/layouts/columns&rows.kt
@@ -1,7 +1,11 @@
 package com.uvg.rueda.laboratorio6.layouts
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
@@ -25,11 +30,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun AppContadora() {
     var contador by remember { mutableStateOf(0)}
@@ -43,7 +50,7 @@ fun AppContadora() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(32.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ){
@@ -68,7 +75,7 @@ fun AppContadora() {
                 },
                 modifier = Modifier.size(60.dp),
                 shape = CircleShape,
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF0071B8))
             ){
                 Text(text = "-", color = Color.White, fontSize = 32.sp)
             }
@@ -89,7 +96,7 @@ fun AppContadora() {
                 },
                 modifier = Modifier.size(60.dp),
                 shape = CircleShape,
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Blue),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF0071B8)),
             ){
                 Text(text = "+", color = Color.White, fontSize = 32.sp)
             }
@@ -100,40 +107,63 @@ fun AppContadora() {
             horizontalAlignment = Alignment.Start
         ){
             Text(text = buildAnnotatedString {
-                append("Total incrementos:            ")
+                append("Total incrementos: ")
                 withStyle(style = SpanStyle(fontWeight = FontWeight.Normal)) {
                     append(incrementroContador.toString())
                 }
             }, fontSize = 28.sp, fontWeight = FontWeight.Bold, color = Color.Black
             )
             Text(text = buildAnnotatedString {
-                append("Total decrementos:           ")
+                append("Total decrementos: ")
                 withStyle(style = SpanStyle(fontWeight = FontWeight.Normal)) {
                     append(decrementoContador.toString())
                 }
             }, fontSize = 28.sp, fontWeight = FontWeight.Bold, color = Color.Black
             )
             Text(text = buildAnnotatedString {
-                append("Valor máximo:                    ")
+                append("Valor máximo: ")
                 withStyle(style = SpanStyle(fontWeight = FontWeight.Normal)) {
                     append(valorMaximo.toString())
                 }
             }, fontSize = 28.sp, fontWeight = FontWeight.Bold, color = Color.Black
             )
             Text(text = buildAnnotatedString {
-                append("Valor mínimo:                    ")
+                append("Valor mínimo: ")
                 withStyle(style = SpanStyle(fontWeight = FontWeight.Normal)) {
                     append(valorMinimo.toString())
                 }
             }, fontSize = 28.sp, fontWeight = FontWeight.Bold, color = Color.Black
             )
             Text(text = buildAnnotatedString {
-                append("Total de cambios:              ")
+                append("Total de cambios: ")
                 withStyle(style = SpanStyle(fontWeight = FontWeight.Normal)) {
                     append(cambioContador.toString())
                 }
             }, fontSize = 28.sp, fontWeight = FontWeight.Bold, color = Color.Black
             )
+            Text(text = "Historial: ", fontSize = 28.sp, fontWeight = FontWeight.Bold, color = Color.Black)
+            FlowRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                maxItemsInEachRow = 5
+            ){
+                historial.forEach{ entry ->
+                    Box(
+                        modifier = Modifier
+                            .size(55.dp)
+                            .background(
+                                color = if (entry.second) Color(0xFF007F23) else Color(0xFFD52300),
+                                shape = RoundedCornerShape(8.dp)
+                            ),
+                        contentAlignment = Alignment.Center
+                    ){
+                        Text(text = entry.first.toString(), color = Color.White, fontSize = 18.sp)
+                    }
+                }
+            }
         }
     }
 }
