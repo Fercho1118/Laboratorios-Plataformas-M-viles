e93163f8fb67910a680d6f5674a8c6d9cea4ef49
Fercho1118 <rue23748@uvg.edu.gt>
2024-08-22 23:05:29 -0600

    TopBar
    Se implementó un TopAppBar para agregar el ícono de flecha y el texto de notificaciones.

--- lab7/app/src/main/java/com/uvg/rueda/lab7/layouts/Columns&Rows.kt
+++ lab7/app/src/main/java/com/uvg/rueda/lab7/layouts/Columns&Rows.kt
@@ -5,8 +5,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -67,6 +73,19 @@ fun generateFakeNotifications(): List<Notification> {
         )
     }
     return notifications
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NotificationAppBar(onBackClick: () -> Unit){
+    TopAppBar(
+        title = { Text(text = "Notificaciones")},
+        navigationIcon = {
+            IconButton(onClick = { onBackClick() }) {
+                Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back" )
+            }
+        }
+    )
 }
 
 @Composable
